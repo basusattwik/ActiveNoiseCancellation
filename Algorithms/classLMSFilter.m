c97100148927f48b3a92ef0b63200ae49dbbac3b
Sattwik Basu <sattwikbasu@wirelessprv-10-193-146-81.near.illinois.edu>
2022-11-05 18:21:41 -0500

    moving class outside, implemented multichannel LMS

--- Algorithms/classLMSFilter.m
+++ Algorithms/classLMSFilter.m
@@ -5,28 +5,31 @@ classdef classLMSFilter < matlab.System
     properties
 
         % system setup
-        
+        numRef = 1;
+        numErr = 1;
 
         % adaptive filter tuning
-        stepsize   = 0.01;
-        leakage    = 0.001;
-        smoothing  = 0.999; 
-        normweight = 1;
-
+        stepsize   = []; % adaptive filter stepsize
+        leakage    = []; % adaptive filter leakage
+        normweight = []; % weight for stepsize normalization factor
+        smoothing  = 0.999; % exponential smoothing constant
+        
         % switches
-        bfreezecoeffs(1, 1)logical = false;
+        bfreezecoeffs(1, 1)logical = false; % bool to freeze coeffients
 
     end
 
     % Public, non-tunable properties
     properties(Nontunable)
-        filterlen(1, 1){mustBePositive, mustBeInteger} = 256; % default value
+        filterlen(1, 1){mustBePositive, mustBeInteger} = 256; % adaptive filter length
     end
 
     properties(DiscreteState)
-        coeffs; 
-        states;  
-        powrefhist;
+        coeffs; % adaptive FIR filter coefficients
+        states; % buffered reference signal
+        error;  % error signal
+        output; % output of each adaptive filter
+        powrefhist; % smoothed power of reference signal
     end
 
     % Pre-computed constants
@@ -46,37 +49,58 @@ classdef classLMSFilter < matlab.System
         %% Common functions
         function setupImpl(obj)
             % Perform one-time calculations, such as computing constants
-            obj.states = zeros(obj.filterlen, 1);
-            obj.coeffs = zeros(obj.filterlen, 1);
-            obj.powrefhist = 0;
+
+            % Probably don't need to keep this here since it is in reset()
+            % already
+            obj.states = zeros(obj.filterlen, obj.numRef);
+            obj.coeffs = zeros(obj.numRef, obj.numErr, obj.filterlen);
+            obj.error  = zeros(1, obj.numErr);
+            obj.output = zeros(obj.numErr, obj.numRef);
+            obj.powrefhist = zeros(1, obj.numRef);
         end
 
-        function [output, error] = stepImpl(obj, ref, des)
+        function stepImpl(obj, ref, des)
             % Implement algorithm. Calculate y as a function of input u and
             % discrete states.
 
             % Update state vector
-            obj.states = [ref; obj.states(1:end-1, 1)];
-        
-            % Get normalized stepsize
-            obj.powrefhist = obj.smoothing * norm(obj.states) + (1 - obj.smoothing) * obj.powrefhist;
-            normstepsize   = obj.stepsize / (1 + obj.normweight * obj.powrefhist);
-        
-            % Get error signal: desired - output
-            output = obj.coeffs.' * obj.states;
-            error  = des - output;
-        
-            % Update filter coefficients
-            if ~obj.bfreezecoeffs
-                obj.coeffs = obj.coeffs * (1 - normstepsize * obj.leakage) + normstepsize * error * obj.states;
+            obj.states = [ref; obj.states(1:end-1, :)];
+
+            % Get output signals
+            for mic = 1:obj.numErr
+                for ref = 1:obj.numRef
+                    obj.output(mic, ref) = squeeze(obj.coeffs(ref, mic, :)).' * obj.states(:, ref);                
+                end
             end
-        end
+
+            % Run LMS update
+            for mic = 1:obj.numErr
+                for ref = 1:obj.numRef
+                
+                % Get error signal: desired - output
+                obj.error(1, mic) = des(1, mic) - sum(obj.output(mic, :));
+
+                % Get normalized stepsize
+                obj.powrefhist(1, ref) = obj.smoothing * norm(obj.states(:, ref)) + (1 - obj.smoothing) * obj.powrefhist(1, ref);
+                normstepsize = obj.stepsize(1, mic) / (1 + obj.normweight(1, mic) * obj.powrefhist(1, ref));
+                      
+                % Update filter coefficients using leaky LMS
+                if ~obj.bfreezecoeffs
+                    obj.coeffs(ref, mic, :) = squeeze(obj.coeffs(ref, mic, :)) * (1 - normstepsize * obj.leakage) ...
+                                                                                    + normstepsize * obj.error(1, mic) * obj.states(:, ref);
+                end
+
+                end % chn loop
+            end % mic loop
+        end 
 
         function resetImpl(obj)
             % Initialize / reset discrete-state properties
-            obj.states = zeros(obj.filterlen, 1);
-            obj.coeffs = zeros(obj.filterlen, 1);
-            obj.powrefhist = 0;
+            obj.states = zeros(obj.filterlen, obj.numRef);
+            obj.coeffs = zeros(obj.numRef, obj.numErr, obj.filterlen);
+            obj.error  = zeros(1, obj.numErr);
+            obj.output = zeros(1, obj.numRef);
+            obj.powrefhist = zeros(1, obj.numRef);
         end
 
         %% Backup/restore functions
