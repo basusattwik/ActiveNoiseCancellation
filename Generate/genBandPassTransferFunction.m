7d8be9f168821ce53e6ce548d5a14797b875e2d1
Sattwik Basu <sattwikbasu@wirelessprv-10-193-146-81.near.illinois.edu>
2022-11-04 20:06:05 -0500

    adding initial example scripts

---
+++ Generate/genBandPassTransferFunction.m
@@ -0,0 +1,17 @@
+function [coeffs] = genBandPassTransferFunction(N, flow, fhigh, delay, Ast, ford, fs)
+%GENBANDPASSTRANSFERFUNCTION Summary of this function goes here
+%   Detailed explanation goes here
+
+% Design bandpass filter to generate bandlimited impulse response
+specs  = fdesign.bandpass('N,Fst1,Fst2,Ast', ford, flow, fhigh, Ast, fs);
+bpfilt = design(specs,'cheby2','FilterStructure','df2tsos', ...
+                       'SystemObject',true);
+
+% Filter noise to generate impulse response
+coeffs = bpfilt([zeros(delay,1); ...
+                              log(0.99 * rand(N-delay, 1) + 0.01) .* ...
+                              sign(randn(N - delay, 1)) .* exp(-0.01 * (1:N-delay)')]);
+
+coeffs = coeffs / norm(coeffs);
+
+end
---
+++ startup.m
@@ -0,0 +1,9 @@
+% Startup script for the ANC simulation framework
+
+close all
+clearvars
+clc
+
+addpath("Examples/");
+addpath("Generate/");
+
